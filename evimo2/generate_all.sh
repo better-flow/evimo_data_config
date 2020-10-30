c219e9256861d7f2cbf1237b06962905ae3577c0
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2020-10-30 11:46:52 -0700

    add data generation scripts

---
+++ evimo2/generate_all.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DATA_ROOT=/home/ncos/evimo_data_config/evimo2
+
+
+./generate.sh $DATA_ROOT/checkerboard_flat_lr
+./generate.sh $DATA_ROOT/simple_depth_var_lr
+./generate.sh $DATA_ROOT/simple_ego_0
+./generate.sh $DATA_ROOT/simple_ego_1
+./generate.sh $DATA_ROOT/sliding_00
+./generate.sh $DATA_ROOT/sliding_01
+./generate.sh $DATA_ROOT/sliding_03
+./generate.sh $DATA_ROOT/sliding_2_00
+./generate.sh $DATA_ROOT/tabletop_flat_rot_z
+./generate.sh $DATA_ROOT/static_objects_00
+./generate.sh $DATA_ROOT/sliding_car_00
