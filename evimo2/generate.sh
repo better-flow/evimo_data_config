0177b00f1fbe64bcf83bf82687ef2b8423e8bfd1
Anton Mitrokhin <anton.mitrokhin@phystech.edu>
2020-10-30 23:13:57 -0700

    add more sequences

--- evimo2/generate.sh
+++ evimo2/generate.sh
@@ -8,17 +8,22 @@ echo "Folder: $1"
 declare -a CameraArray=("flea3_7" "samsung_mono" "left_camera" "right_camera")
 for cam in ${CameraArray[@]}; do
     echo $cam
+
+    S_NAME=$(basename $1)
+
     roslaunch evimo event_imo_offline.launch show:=-1 folder:=$1 camera_name:=$cam \
                                              generate:=true \
                                              save_3d:=false \
                                              fps:=40 \
-                                            t_offset:=0 \
+                                             t_offset:=0 \
                                              t_len:=-1
 
     python3 $PYDVS_DIR/samples/evimo-gen.py --base_dir $1/$cam/ground_truth
 
     VIS_FOLDER=$1/$cam/ground_truth/vis
-    VIDEO_DST=$1/${cam}.mp4
+    VIDEO_DST=$1/${S_NAME}_${cam}.mp4
+    rm $VIDEO_DST
+
     ffmpeg -r 40 -i $VIS_FOLDER/frame_%10d.png -c:v libx264 -vf \
         "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p \
         $VIDEO_DST
--- evimo2/generate_all.sh
+++ evimo2/generate_all.sh
@@ -12,5 +12,10 @@ DATA_ROOT=/home/ncos/evimo_data_config/evimo2
 ./generate.sh $DATA_ROOT/sliding_03
 ./generate.sh $DATA_ROOT/sliding_2_00
 ./generate.sh $DATA_ROOT/tabletop_flat_rot_z
-./generate.sh $DATA_ROOT/static_objects_00
 ./generate.sh $DATA_ROOT/sliding_car_00
+./generate.sh $DATA_ROOT/static_objects_00
+./generate.sh $DATA_ROOT/static_objects_01
+./generate.sh $DATA_ROOT/static_objects_02
+./generate.sh $DATA_ROOT/static_objects_03
+./generate.sh $DATA_ROOT/falling_objects_00
+./generate.sh $DATA_ROOT/dynamic_00
